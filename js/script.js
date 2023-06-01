230b7842b37a5f7a758ebc1fb58e4451939cd3d9
Salem Ali <salemali.worku@gmail.com>
2023-06-01 13:22:07 -0300

    light-mode

--- js/script.js
+++ js/script.js
@@ -23,3 +23,74 @@ function showPassword() {
     icon.setAttribute("name", "eye-outline");
   }
 }
+
+fetch("https://api.rawg.io/api/games?key=5ddfec9274094f7c93c073204ccecff1")
+  .then((response) => response.json())
+  .then((data) => {
+    let api = data.results;
+    cardStats(api, "#card-template");
+  });
+
+const gamesList =
+  "https://api.rawg.io/api/games?key=5ddfec9274094f7c93c073204ccecff1";
+
+function cardStats(Array, idContainer) {
+  let container = document.querySelector(idContainer);
+  console.log(Array);
+  Array = orderByRating(Array);
+  Array.forEach((x, i) => {
+    let div = document.createElement("div");
+    div.className = "card";
+    div.innerHTML = `
+    <div class="card-part-1">
+      <img src="${x.background_image}" alt="Filter">
+      <ion-icon name="heart-outline"></ion-icon>
+    </div>
+    <div class="card-part-2">
+      <div class="card-title">
+        <h2>${x.name}</h2>
+        <p>#${i + 1}</p>
+      </div>
+      <div class="card-data">
+        <div class="release">
+          <p class="grey-text">Release date: </p>
+          <p >${x.released}</p>
+          <div class="card-icons">
+            <ion-icon name="logo-playstation"></ion-icon>
+            <ion-icon name="logo-xbox"></ion-icon>
+            <ion-icon name="logo-windows"></ion-icon>
+            <img src="/img/nintendo.svg" alt="">
+          </div>
+        </div>
+        <div class="card-info">
+          <p class="grey-text">Genres: </p>
+          <p>${getGenres(x.genres)}</p>
+        </div> 
+        </div>
+        </div>`;
+    container.appendChild(div);
+  });
+}
+
+function getGenres(Array) {
+  let genres = "";
+  Array.forEach((element) => {
+    genres += element.name;
+    genres += " ";
+  });
+  return genres;
+}
+
+function orderByRating(games) {
+  games.sort((a, b) => b.rating - a.rating);
+  return games;
+}
+
+
+const btnSwitch = document.querySelector('#checkbox')
+const header = document.querySelector('header')
+
+btnSwitch.addEventListener('click', ()=>{
+  document.body.classList.toggle('white')
+  header.classList.toggle('white')
+})
